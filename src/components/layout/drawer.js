916386c07804992b77eb387510e9ab05cb268ebe
Andrew <a.a.zgara@gmail.com>
2018-08-10 11:44:25 +0300

    добавил слжный список  drawer

--- src/components/layout/drawer.js
+++ src/components/layout/drawer.js
@@ -15,10 +15,22 @@ import ApplicationIcon from '@material-ui/icons/Description';
 import ContactIcon from '@material-ui/icons/Info';
 import en from "react-intl/locale-data/en";
 import ru from "react-intl/locale-data/ru";
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import List from '@material-ui/core/List';
+import StarBorder from '@material-ui/icons/StarBorder';
+import Divider from "@material-ui/core/es/Divider/Divider";
 
 addLocaleData([...en, ...ru]);
 
 class LocaleDrawer extends React.Component {
+	state = { open: true };
+
+	handleClick = () => {
+		this.setState(state => ({ open: !state.open }));
+	};
+
 	render() {
 		return <IntlProvider
 			locale={this.props.locale}
@@ -37,12 +49,66 @@ class LocaleDrawer extends React.Component {
 					</ListItemIcon>
 					<ListItemText primary={<FormattedMessage id="drawerList.textItem2"/>} />
 				</ListItem>
-				<ListItem button component={Link} to={'/partners'}>
+				<ListItem button component={Link} to={'/partners'} onClick={this.handleClick}>
 					<ListItemIcon>
 						<PartnersIcon />
 					</ListItemIcon>
 					<ListItemText primary={<FormattedMessage id="drawerList.textItem3"/>} />
+					{this.state.open ? <ExpandLess /> : <ExpandMore />}
 				</ListItem>
+				<Collapse in={this.state.open} timeout="auto" unmountOnExit>
+					<List component="partnersId" disablePadding >
+						<Divider inset />
+						<ListItem button style={{paddingLeft: '40px'}}>
+							<ListItemIcon>
+								<StarBorder />
+							</ListItemIcon>
+							<ListItemText inset primary="SAMOS" />
+						</ListItem>
+								<List component="samosId" disablePadding >
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="CERTIFICATION " />
+									</ListItem>
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="CADETS" />
+									</ListItem>
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="MEDICAL" />
+									</ListItem>
+								</List>
+						<Divider inset />
+						<ListItem button style={{paddingLeft: '40px'}}>
+							<ListItemIcon>
+								<StarBorder />
+							</ListItemIcon>
+							<ListItemText inset primary="NOMIKOS" />
+						</ListItem>
+								<List component="nomikosId" disablePadding >
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="CERTIFICATION " />
+									</ListItem>
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="MEDICAL" />
+									</ListItem>
+								</List>
+						<Divider inset />
+						<ListItem button style={{paddingLeft: '40px'}}>
+							<ListItemIcon>
+								<StarBorder />
+							</ListItemIcon>
+							<ListItemText inset primary="OFFEN" />
+						</ListItem>
+								<List component="offenId" disablePadding >
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="CERTIFICATION " />
+									</ListItem>
+									<ListItem button style={{padding: '5px'}}>
+										<ListItemText inset secondary="MEDICAL" />
+									</ListItem>
+								</List>
+						<Divider inset />
+					</List>
+				</Collapse>
 				<ListItem button component={Link} to={'/vacancies'}>
 					<ListItemIcon>
 						<VacanciesIcon />
