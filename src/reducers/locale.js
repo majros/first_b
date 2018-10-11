0851f412125341045e74826a8553ba8320b7845a
Kostya Zgara <kostyazgara2@gmail.com>
2018-10-11 22:23:27 +0300

    fix undefined locale in dict

--- src/reducers/locale.js
+++ src/reducers/locale.js
@@ -1,12 +1,16 @@
 import {CHANGE_LOCALE} from "../actions/types";
+import { messages } from "../dictionary/messages";
 
 const initialLocale = (navigator.languages && navigator.languages[0])
 	|| navigator.language
 	|| navigator.userLanguage
 	|| 'ru-RU';
 
+// need add check if locale exits in your dictionary.
+// because in my chrome, locale equal just "en" and this locale doesn't exists in your dictionary, only "en-US"
+// if locale doesn't exits fallback to "ru-RU", it's enough for simple usage, but in future need add suffix or something else
 const initState = {
-	currentLanguage: initialLocale
+	currentLanguage: messages[initialLocale] ? initialLocale : 'ru-RU'
 };
 
 export default (state = initState, action) => {
